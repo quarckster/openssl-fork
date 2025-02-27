openssl/openssl

Multi-variant ML-KEM
This introduces support for ML-KEM-512 and ML-KEM-1024 using the same
underlying implementation parameterised by a few macros for the
associated types and constants.

KAT tests are added for ML-KEM 512 and 1024, to complement the previous
tests for ML-KEM-768.

MLKEM TLS group codepoints are updated to make the additional KEMs
known to the TLS layer (ML-KEM is not enabled by default).

Most of the underlying code is shared, but the external
generate/encap/decap API is variant-specific, and uses
some implicit buffer sizes and dedicated key types.

Variant-specific) code is compiled separately for each of ML-KEM-512,
ML-KEM-768 and ML-KEM-1024 from a common source file, which contains
short wrappers for the shared code.

There is an additional API that takes an "const ossl_ml_kem_vinfo"
pointer, (obtained from ossl_ml_kem_*_get_vinfo()).  This checks the
otherwise implicit sizes and uses (void *) key pointers, converting them
to the appropriate type before calling the right variant-specific API.
This is used to simplify the code in the provider, which other than
initialising the right variant parameters is then share across all the
variants.

The original BoringSSL code was refactored to eliminate the opaque
public/private key structure wrappers, since these structures will be
private to the provider implementation, and are not part of the public
API.

Support for ML-KEM-512 required adding a centered binomial distribution
helper function to deal with η_1 == 3 in that algorithm.

There were some unrelated literal 32's and 64's in the code, these were
replaced by the appropriate macros.

Some additional comments were added to highlight how the code relates to
the ML-KEM specification in FIPS 203.

Some tests were added to excercise also the non-derandomised ML-KEM
APIs, but so far the "vinfo" driven functions above the provider level,
the provider-layer (end-to-end) non-derandomised tests are yet to be
written, but should be fairly tractable.

providers/defltprov.c
@@ -477,7 +477,11 @@ static const OSSL_ALGORITHM deflt_asym_kem[] = {
 # endif
     { PROV_NAMES_EC, "provider=default", ossl_ec_asym_kem_functions },
 #endif
-    { PROV_NAMES_MLKEM768, "provider=default", ossl_mlkem768_asym_kem_functions },
+#ifndef OPENSSL_NO_ML_KEM
+    { PROV_NAMES_ML_KEM_512, "provider=default", ossl_ml_kem_512_asym_kem_functions },
+    { PROV_NAMES_ML_KEM_768, "provider=default", ossl_ml_kem_768_asym_kem_functions },
+    { PROV_NAMES_ML_KEM_1024, "provider=default", ossl_ml_kem_1024_asym_kem_functions },
+#endif
     { NULL, NULL, NULL }
 };
 
@@ -532,8 +536,12 @@ static const OSSL_ALGORITHM deflt_keymgmt[] = {
     { PROV_NAMES_SM2, "provider=default", ossl_sm2_keymgmt_functions,
       PROV_DESCS_SM2 },
 #endif
-    { PROV_NAMES_MLKEM768, "provider=default", ossl_mlkem768_keymgmt_functions,
-      PROV_DESCS_MLKEM768 },
+    { PROV_NAMES_ML_KEM_512, "provider=default", ossl_ml_kem_512_keymgmt_functions,
+      PROV_DESCS_ML_KEM_512 },
+    { PROV_NAMES_ML_KEM_768, "provider=default", ossl_ml_kem_768_keymgmt_functions,
+      PROV_DESCS_ML_KEM_768 },
+    { PROV_NAMES_ML_KEM_1024, "provider=default", ossl_ml_kem_1024_keymgmt_functions,
+      PROV_DESCS_ML_KEM_1024 },
     { NULL, NULL, NULL }
 };
 

quarckster/openssl-fork

MLKEM{512,768,1024} TLS "group" codepoints are updated to match the
final IANA assigments and to make the additional KEMs known to the TLS
layer.
The pure-QC MLKEMs are not in the default list of supported groups, and
need to be explicitly enabled by the application.  Future work will
introduce support for hybrids, and for more fine-grained policy of
which keyshares a client should send by default, and when a server
should request (HRR) a new mutually-supported group that was not
sent.
Tests for ML-KEM key exchange added to sslapitest to make sure that our
TLS client MLKEM{512,768,1024} implementations interoperate with our TLS
server, and that MLKEM* are not negotiated in TLS 1.2.
Tests also added to excercise non-derandomised ML-KEM APIs, both
directly (bypassing the provider layer), and through the generic EVP KEM
API (exercising the provider).  These make sure that RNG input is used
correctly (KAT tests bypass the RNG by specifying seeds).
The API interface to the provider takes an "const ML_KEM_VINFO" pointer,
(obtained from ossl_ml_kem_get_vinfo()).  This checks input and output
buffer sizes before passing control to internal code that assumes
correctly sized (for each variant) buffers.
The original BoringSSL API was refactored to eliminate the opaque
public/private key structure wrappers, since these structures are an
internal detail between libcrypto and the provider, they are not part of
the public (EVP) API.
New "clangover" counter-measures added, refined with much appreciated
input from David Benjamin (Chromium).
The internal steps of "encrypt_cpa" were reordered to reduce the
working-set size of the algorithm, now needs space for just two
temporary "vectors" rather than three.  The "decap" function now process
the decrypted message in one call, rather than three separate calls to
scalar_decode_1, scalar_decompress and scalar_add.
Some loops were unrolled, improving performance of en/decapsulate
(pre-expanded vectors and matrix) by around 5%.
To handle, h

providers/defltprov.c
@@ -482,7 +482,11 @@ static const OSSL_ALGORITHM deflt_asym_kem[] = {
 # endif
     { PROV_NAMES_EC, "provider=default", ossl_ec_asym_kem_functions },
 #endif
-    { PROV_NAMES_MLKEM768, "provider=default", ossl_mlkem768_asym_kem_functions },
+#ifndef OPENSSL_NO_ML_KEM
+    { PROV_NAMES_ML_KEM_512, "provider=default", ossl_ml_kem_asym_kem_functions },
+    { PROV_NAMES_ML_KEM_768, "provider=default", ossl_ml_kem_asym_kem_functions },
+    { PROV_NAMES_ML_KEM_1024, "provider=default", ossl_ml_kem_asym_kem_functions },
+#endif
     { NULL, NULL, NULL }
 };
 
@@ -545,8 +549,14 @@ static const OSSL_ALGORITHM deflt_keymgmt[] = {
     { PROV_NAMES_SM2, "provider=default", ossl_sm2_keymgmt_functions,
       PROV_DESCS_SM2 },
 #endif
-    { PROV_NAMES_MLKEM768, "provider=default", ossl_mlkem768_keymgmt_functions,
-      PROV_DESCS_MLKEM768 },
+#ifndef OPENSSL_NO_ML_KEM
+    { PROV_NAMES_ML_KEM_512, "provider=default", ossl_ml_kem_512_keymgmt_functions,
+      PROV_DESCS_ML_KEM_512 },
+    { PROV_NAMES_ML_KEM_768, "provider=default", ossl_ml_kem_768_keymgmt_functions,
+      PROV_DESCS_ML_KEM_768 },
+    { PROV_NAMES_ML_KEM_1024, "provider=default", ossl_ml_kem_1024_keymgmt_functions,
+      PROV_DESCS_ML_KEM_1024 },
+#endif
     { NULL, NULL, NULL }
 };
 